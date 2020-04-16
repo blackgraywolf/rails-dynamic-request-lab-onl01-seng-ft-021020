66f96a50005bfd2a48d41f04c6a63834504f10e8
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-16 20:36:23 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+<<<<<<< HEAD
    get 'students' => 'students#index'
    get 'students/:id', to: 'students#show'
    
+=======
+  resources :students, only: :index
+  get 'students', to: 'students#index'
+>>>>>>> 9b3fdef88963059ef9d75bb2b9f9ce88995cc537
 end
